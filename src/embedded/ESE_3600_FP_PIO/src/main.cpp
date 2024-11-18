24cdf40101dccca177c619ad24519b5f223f2751
rp-seas <157322887+rp-seas@users.noreply.github.com>
2024-11-18 13:07:29 -0500

    updated file directories

--- src/embedded/ESE_3600_FP_PIO/src/main.cpp
+++ src/embedded/ESE_3600_FP_PIO/src/main.cpp
@@ -1,14 +1,16 @@
 #include <Arduino.h>
 
-void setup()
-{
-  pinMode(LED_BUILTIN, OUTPUT);
-}
+#include "data_collection.ino"
 
-void loop()
-{
-  digitalWrite(LED_BUILTIN, HIGH);
-  delay(1000);
-  digitalWrite(LED_BUILTIN, LOW);
-  delay(1000);
-}
+// void setup()
+// {
+//   pinMode(LED_BUILTIN, OUTPUT);
+// }
+
+// void loop()
+// {
+//   digitalWrite(LED_BUILTIN, HIGH);
+//   delay(1000);
+//   digitalWrite(LED_BUILTIN, LOW);
+//   delay(1000);
+// }
