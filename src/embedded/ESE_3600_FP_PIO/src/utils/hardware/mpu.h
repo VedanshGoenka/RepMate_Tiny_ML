5136da26f2060658521bed2bf4a980d1cb190a3f
Vedansh Goenka <vedanshg@seas.upenn.edu>
2024-11-29 03:16:46 -0800

    IMU -> Buffer

--- src/embedded/ESE_3600_FP_PIO/src/utils/hardware/mpu.h
+++ src/embedded/ESE_3600_FP_PIO/src/utils/hardware/mpu.h
@@ -2,5 +2,4 @@
 
 #include <Adafruit_MPU6050.h>
 
-// Declare MPU as extern
 extern Adafruit_MPU6050 mpu; 
--- src/embedded/ESE_3600_FP_PIO/src/utils/tflite/imu_provider.cpp
+++ src/embedded/ESE_3600_FP_PIO/src/utils/tflite/imu_provider.cpp
@@ -3,3 +3,54 @@
 
 const int target_samples = 50;      // Number of samples to collect (50 * 5 ms = 250 ms)
 const int sampling_interval_ms = 2; // Interval between samples
+
+void imuSetup()
+{
+  // Initialize MPU6050
+  while (!mpu.begin())
+  {
+    delay(500);
+  }
+
+  mpu.setAccelerometerRange(MPU6050_RANGE_8_G);
+  mpu.setGyroRange(MPU6050_RANGE_500_DEG);
+  mpu.setFilterBandwidth(MPU6050_BAND_21_HZ);
+}
+
+void imuCollect(CircularBuffer<TimeSeriesDataPoint> &buffer)
+{
+
+  for (int i = 0; i < target_samples; ++i)
+  {
+    // Fetch IMU data
+    sensors_event_t accel, gyro, temp;
+    mpu.getEvent(&accel, &gyro, &temp);
+
+    // Add data to the buffer
+    addDataToBuffer(
+        buffer,
+        accel.acceleration.x,
+        accel.acceleration.y,
+        accel.acceleration.z,
+        gyro.gyro.x,
+        gyro.gyro.y,
+        gyro.gyro.z);
+
+    // Wait for the next sampling interval
+    delay(sampling_interval_ms);
+  }
+}
+
+void addDataToBuffer(CircularBuffer<TimeSeriesDataPoint> &buffer, float aX, float aY, float aZ, float gX, float gY, float gZ)
+{
+  TimeSeriesDataPoint dataPoint = {
+      .aX = static_cast<float>(aX),
+      .aY = static_cast<float>(aY),
+      .aZ = static_cast<float>(aZ),
+      .gX = static_cast<float>(gX),
+      .gY = static_cast<float>(gY),
+      .gZ = static_cast<float>(gZ)};
+
+  // Add to circular buffer
+  buffer.push(dataPoint);
+}
